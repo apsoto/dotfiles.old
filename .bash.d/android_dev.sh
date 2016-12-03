53e903a548e6a234ad07df6dc56e3960d6e7675b
Alex Soto <apsoto@gmail.com>
2016-12-03 13:50:09 -0800

    delete some files I no longer need

--- .bash.d/android_dev.sh
+++
@@ -1,3 +0,0 @@
-export ANDROID_HOME=~/projects/android-sdk-mac_x86
-export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
-export ANDROID_LOG_TAGS="APP:D *:E"
--- .bash.d/git.sh
+++
@@ -1,4 +0,0 @@
-if [ -f /opt/local/etc/bash_completion.d/git ]; then
-    . /opt/local/etc/bash_completion.d/git
-fi
-
