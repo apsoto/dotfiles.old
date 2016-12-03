ad3176542a7fe5bff802ed5c92935a16c7582bf9
Alex Soto <apsoto@gmail.com>
2016-12-03 13:54:18 -0800

    switched to brew for bash completion

--- .bash.d/completion.sh
+++ .bash.d/completion.sh
@@ -1,14 +1,6 @@
 # enable programmable completion features (you don't need to enable
 # this, if it's already enabled in /etc/bash.bashrc and /etc/profile
 # sources /etc/bash.bashrc).
-if [ -f /opt/local/etc/bash_completion ]; then
-    . /opt/local/etc/bash_completion
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+    . $(brew --prefix)/etc/bash_completion
 fi
-
-#if [ -f ~/.bash_completion ]; then
-#    . ~/.bash_completion
-#fi
-
-
-# complete host aliases in my ssh config
-#complete -o default -o nospace -W "$(/usr/bin/env ruby -ne 'puts $_.split(/[,\s]+/)[1..-1].reject{|host| host.match(/\*|\?/)} if $_.match(/^\s*Host\s+/);' < $HOME/.ssh/config)" scp sftp ssh
