5a1da0794b3ba33b7007c8b5498b26d0778d7ccd
Alex Soto <apsoto@gmail.com>
2016-12-03 13:57:38 -0800

    switch to sublime for editor

--- .bash.d/environment.sh
+++ .bash.d/environment.sh
@@ -1,9 +1,9 @@
 #
 # setup environment variables
 #
-export EDITOR="/Users/asoto/.bin/mvim -f"
-export DIFF="/opt/local/bin/colordiff"
+#export EDITOR="/Users/asoto/.bin/mvim -f"
+export EDITOR="/usr/local/bin/subl -n -w"
+export DIFF="/usr/local/bin/colordiff"
 export MERGE="/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge"
 export LC_ALL=en_US.UTF-8
 export LSCOLORS='Dxfxcxdxbxegedabagacad'
-
