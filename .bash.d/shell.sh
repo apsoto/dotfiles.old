7fae268cb42ca597f86fee644f9152f9452fb26b
Alex Soto <apsoto@gmail.com>
2016-12-05 11:47:48 -0800

    use CDPATH instead of the "z" tool to jump around

--- .bash.d/shell.sh
+++ .bash.d/shell.sh
@@ -5,3 +5,9 @@ shopt -s checkwinsize
 # vi mode for editing command lines.
 set -o vi
 
+# CDPATH so you can jump around without typing a lot.  Uses autocompletion too
+export CDPATH=.:~
+cdpaths="$([ -d ~/projects ] && find ~/projects -maxdepth 1 -type d | egrep -v '/(\.)|_[a-zA-Z0-9]' | egrep -v '(bin)|(cmd)|(doc)|(lib)|(pkg)|(test)' | xargs -n1 dirname | uniq)"
+for i in $cdpaths; do
+  CDPATH=$CDPATH:$i
+done
